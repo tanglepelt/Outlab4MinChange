96b42ff1417c684c94dee184b6455cc97c257692
gabby70215 <37000387+gabby70215@users.noreply.github.com>
2018-04-22 16:24:55 -0600

    changes
    tried adding another coin system, freaked it out so took it out.

--- test/JUnitRun.java
+++ test/JUnitRun.java
@@ -1,5 +1,13 @@
+/*
+ *Brittany Couts, Gabby Strong, Brock
+ *4/20/2018
+ *
+ *This is the tester
+*/
+
 import minchange.outlab4.CoinChanging;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 
@@ -7,44 +15,52 @@ public class JUnitRun
 { 
     int[] UScoins = { 1, 5, 10, 25 };
     //test one test coins 42
-    @Test
+    @Test //(expected = IllegalArgumentException.class)
     public void test1()
     {
         int[] expected = {1,1,5,10,25};
-        
         int coins = 42;
         Assert.assertArrayEquals(expected,CoinChanging.run(UScoins,coins));
     }
-    
     //test with 50 cents
-    @Test
+    @Test //(expected = IllegalArgumentException.class)
     public void test2()
     {
         int[] expected = {25,25};
-
         int coins = 50;
         Assert.assertArrayEquals(expected,CoinChanging.run(UScoins,coins));
     } 
-    
     //test of 5
-    @Test
+    @Test //(expected = IllegalArgumentException.class)
     public void test3()
     {
         int[] expected = {5};
-
         int coins = 5;
         Assert.assertArrayEquals(expected,CoinChanging.run(UScoins,coins));
     }
-    
+   
+    // test 112 coin
+    @Test //(expected = IllegalArgumentException.class)
+    public void bigCoinTest()
+    {
+        int[] expected = {1,1,10,25,25,25,25};
+        int coins = 112;
+        Assert.assertArrayEquals(expected,CoinChanging.run(UScoins,coins));
+    }
+    //test of 1016
+    @Test //(expected = IllegalArgumentException.class)
+    public void biggerCoinTest()
+    {
+        int[] expected = {1,5,10,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25};
+        int coins = 1016;
+        Assert.assertArrayEquals(expected,CoinChanging.run(UScoins,coins));
+    }
     //testing empty array
-    @Test(expected = IllegalArgumentException.class)
+     @Test//(expected = IllegalArgumentException.class)
     public void EmptyArray()
     {
-        
         int array[] = {};
         int coins = 10;
         Assert.assertArrayEquals(null,CoinChanging.run(array,coins));
-        
     }
-
 }
